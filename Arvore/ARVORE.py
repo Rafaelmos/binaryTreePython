456554b7227d4ef3910de30985f8cce7c5327f22
Rafael Moura <65968331+Rafaelmos@users.noreply.github.com>
2020-05-30 01:08:26 -0300

    Atualização do Sucessor e Antecessor

--- Arvore/ARVORE.py
+++ Arvore/ARVORE.py
@@ -6,6 +6,7 @@ class No():
         self.direita = None
         self.esquerda = None
         self.pai = None
+
     def __str__(self):
         return f'NO: {self.valor}'
 
@@ -14,61 +15,8 @@ class Arvore():
         self.raiz = None
         self.quantidadeFolhas = 0
 
-    def menu(self):
-        while True:
-            print("""                                           \033[1;30mMENU\033[0;0m	
-            [1]  Imprimir Arvore      |  [2]  Buscar      |  [3] Adicionar a Arvore         | 
-            [4]  Maximo               |  [5]  Minimo      |  [6] Quantidade de Nos          |
-            [7]  Sucessor             |  [8]  Antecessor  |  [9] Adicionar lista a Arvore   |
-            [10] Arvore para lista    |  [11] Remover     |  [0] SAIR                       | \n""")
-            botao = input("Escolha a Opção: ")
-            if botao == "0":
-                print("\033[1;31mPrograma Finalizado\033[0;0m	")
-                return
-            elif botao == "1":
-                arvore.imprimir()
-
-            elif botao == "2":
-                pp = int((input("Digite o valor a ser procurado: ")))
-                print(arvore.buscar(pp))
-                #return
-            elif botao == "3":
-                pp = int((input("Digite o adicionado: ")))
-                arvore.add(pp)
-                print("Ação realizada")
-                #return
-            elif botao == "4":
-                print(f"O Maximo é: {arvore.maximo(arvore.raiz)}")
-                #return
-            elif botao == "5":
-                print(f"O Mínimo é: {arvore.minimo(arvore.raiz)}")
-                #return
-            elif botao == "6":
-                print("Quantidade de folhas:")
-                print(arvore.quantidadeFolhas)
-                #return
-            elif botao == "7":
-                print("Não finalizado")
-                #return
-            elif botao == "8":
-                print("Não finalizado")
-                #return
-            elif botao == "9":
-                arvore.setList()
-                #return
-            elif botao == "10":
-                print (f"A Lista da Arvore é \n{arvore.getList()}")
-                #return
-            elif botao == "11":
-                print(f"Possiveis nós deletaveis: {arvore.getList()}")
-                deletar = int(input("Digite o nó que você deseja deletar: "))
-                arvore.remover(deletar)
-                print("Nó removido")
-                arvore.imprimir()
-            else:
-                print("\033[1;31mBotão Inexistente\033[0;0m	")
-                #return
     def __addNo(self, no, perc):
+
         if no.valor>perc.valor:
             if perc.direita==None:
                 perc.direita = no
@@ -79,9 +27,8 @@ class Arvore():
                 perc.esquerda = no
             else:
                 self.__addNo( no, perc.esquerda)
-
         else:
-            raise ("numero já existe")
+            raise Exception("Valor já existe")
 
     def add(self, valor):
         no = No(valor)
@@ -91,8 +38,7 @@ class Arvore():
             self.__addNo(no, self.raiz)
         self.quantidadeFolhas+=1
 
-    def __buscarNo(self, valor, percorredor):
-        perc = percorredor
+    def __buscarNo(self, valor, perc):
         if perc == None:
             return None
         if perc.valor == valor:
@@ -108,7 +54,7 @@ class Arvore():
             return "Valor não existe na árvore"
         busca = self.__buscarNo(valor, self.raiz)
         if busca == None:
-            return "Valor não existe árvore"
+            return "Valor não existe na árvore"
         return "Valor encontrado"
 
     def __imprimir(self, perc):
@@ -121,70 +67,129 @@ class Arvore():
         self.__imprimir(self.raiz)
 
     def minimo(self, perc):
-        while perc.esquerda is not None:
+        while perc.esquerda != None:
             perc = perc.esquerda
         return perc
 
     def maximo(self, perc):
-        while perc.direita is not None:
+        while perc.direita != None:
             perc = perc.direita
         return perc
 
-    def sucessor(self, perc):
-        pass
+    def sucessor(self):
+        sucessorlista = list()
 
-    def antecessor(self):
-        pass
+        def _sucessor(atual):
+            if atual != None:
+                _sucessor(atual.esquerda)
+                sucessorlista.append(atual.valor)
+                _sucessor(atual.direita)
+
+        _sucessor(self.raiz)
+        v = int(input("Digite o valor a ser procurado: "))
+        if v not in sucessorlista:
+            print("valor nao existe na arvore")
+        else:
+            indice = sucessorlista.index(v)
+            antecessor_ = indice - 1
+            tamanho = len(sucessorlista)
+            sucessor_ = indice + 1
+            if sucessor_ >= tamanho:
+                print(f"Sucessor não exite")
+            elif antecessor_ <= tamanho - (tamanho + 1):
+                print(f"Sucessor de {v} é {sucessorlista[sucessor_]}")
+            elif antecessor_ >= tamanho - (tamanho + 1) and tamanho >= sucessor_:
+                print(f"Sucessor de {v} é {sucessorlista[sucessor_]}")
+
+    def antecessor(self, ):
+        antecessorlista = list()
+
+        def _antecessor(atual):
+            if atual != None:
+                _antecessor(atual.esquerda)
+                antecessorlista.append(atual.valor)
+                _antecessor(atual.direita)
+
+        _antecessor(self.raiz)
+        v = int(input("Digite o valor a ser procurado: "))
+        if v not in antecessorlista:
+            print("valor nao existe na lista")
+        else:
+            indice = antecessorlista.index(v)
+            antecessor_ = indice - 1
+            tamanho = len(antecessorlista)
+            sucessor_ = indice + 1
+            if sucessor_ >= tamanho:
+                print(f"Antecessor de {v} é {antecessorlista[antecessor_]}")
+            elif antecessor_ <= tamanho - (tamanho + 1):
+                print(f"Antecessor não exite")
+            elif antecessor_ >= tamanho - (tamanho + 1) and tamanho >= sucessor_:
+                print(f"Antecessor de {v} é {antecessorlista[antecessor_]}")
 
     def remover(self, valor):
         if self.raiz == None:
             raise ValueError("lista Vazia")
         perc = self.raiz
-        dad_no = None
+        pai_no = None
         while perc.valor != valor:
-            dad_no = perc
+            pai_no = perc
             if valor < perc.valor:
                 perc = perc.esquerda
             else:
                 perc = perc.direita
-        if perc == None:
-            return False
-        if perc.esquerda is None and perc.direita is None:
+        if perc.esquerda == None and perc.direita == None:
             if perc == self.raiz:
                 self.raiz = None
             else:
-                if dad_no.esquerda == perc:
-                    dad_no.esquerda = None
-
+                if pai_no.esquerda == perc:
+                    pai_no.esquerda = None
                 else:
-                    dad_no.direita = None
+                    pai_no.direita = None
         elif perc.direita == None:
             if perc == self.raiz:
                 self.raiz = perc.esquerda
             else:
-                if dad_no.esquerda == perc:
-                    dad_no.esquerda = perc.esquerda
+                if pai_no.esquerda == perc:
+                    pai_no.esquerda = perc.esquerda
                 else:
-                    dad_no.direita = perc.direita
+                    pai_no.direita = perc.direita
         elif perc.esquerda == None:
             if perc == self.raiz:
                 self.raiz = perc.direita
             else:
-                if dad_no.direita == perc:
-                    dad_no.direita = perc.direita
+                if pai_no.direita == perc:
+                    pai_no.direita = perc.direita
                 else:
-                    dad_no.esquerda = perc.esquerda
-
+                    pai_no.esquerda = perc.esquerda
 
     def getList(self):
-        Lista = list ()
-        def Listar(atual):
-            if atual is not None:
-                Listar(atual.esquerda)
+        Lista = list()
+        Lista2 = list()
+        Lista3 = list()
+
+        def InOrdem(atual):
+            if atual != None:
+                InOrdem(atual.esquerda)
                 Lista.append(atual.valor)
-                Listar(atual.direita)
-        Listar(self.raiz)
-        return Lista
+                InOrdem(atual.direita)
+
+        def preOrdem(atual):
+            if atual != None:
+                Lista2.append(atual.valor)
+                preOrdem(atual.esquerda)
+                preOrdem(atual.direita)
+
+        def posOrdem(atual):
+            if atual != None:
+                posOrdem(atual.esquerda)
+                posOrdem(atual.direita)
+                Lista3.append(atual.valor)
+
+        InOrdem(self.raiz)
+        preOrdem(self.raiz)
+        posOrdem(self.raiz)
+        print (f"Lista em InOrdem {Lista}\nLista em preOrdem {Lista2}\nLista em posOrdem {Lista3}")
+        return
 
     def setList(self):
         listaAddArvore = list()
@@ -209,10 +214,65 @@ class Arvore():
         else:
             print("Ocorreu um erro, tente novamente")
 
+    def Listaremover(self):
+        apagar = list()
+        def apagador(atual):
+            if atual != None:
+                apagador(atual.esquerda)
+
+                apagar.append(atual.valor)
+                apagador(atual.direita)
+        apagador(self.raiz)
+        return apagar
+
+    def menu(self):
+        while True:
+            print("""                                           \033[1;30mMENU\033[0;0m	
+            |[1]  Imprimir Arvore      |  [2]  Buscar      |  [3] Adicionar a Arvore         | 
+            |[4]  Maximo               |  [5]  Minimo      |  [6] Quantidade de Nos          |
+            |[7]  Sucessor             |  [8]  Antecessor  |  [9] Adicionar lista a Arvore   |
+            |[10] Arvore para lista    |  [11] Remover     |  [0] SAIR                       | \n""")
+            botao = input("Escolha a Opção: ")
+            if botao == "0":
+                print("\033[1;31mPrograma Finalizado\033[0;0m	")
+                return
+            elif botao == "1":
+                arvore.imprimir()
+            elif botao == "2":
+                pp = int((input("Digite o valor a ser procurado: ")))
+                print(arvore.buscar(pp))
+            elif botao == "3":
+                pp = int((input("Digite o adicionado: ")))
+                arvore.add(pp)
+                print("Ação realizada")
+            elif botao == "4":
+                print(f"O Maximo é: {arvore.maximo(arvore.raiz)}")
+            elif botao == "5":
+                print(f"O Mínimo é: {arvore.minimo(arvore.raiz)}")
+            elif botao == "6":
+                print("Quantidade de folhas:")
+                print(arvore.quantidadeFolhas)
+            elif botao == "7":
+                arvore.sucessor()
+            elif botao == "8":
+                arvore.antecessor()
+            elif botao == "9":
+                arvore.setList()
+            elif botao == "10":
+                arvore.getList()
+            elif botao == "11":
+                print(f"Possiveis nós deletaveis: {arvore.Listaremover()}")
+                deletar = int(input("Digite o nó que você deseja deletar: "))
+                arvore.remover(deletar)
+                print("Nó removido")
+                arvore.imprimir()
+            else:
+                print("\033[1;31mBotão Inexistente\033[0;0m	")
 
 
 arvore = Arvore()
 arvore.add(100)
+arvore.add(101)
 arvore.add(200)
 arvore.add(180)
 arvore.add(70)
@@ -221,8 +281,10 @@ arvore.add(65)
 arvore.add(50)
 arvore.menu()
 
+
+
 '''Buscar'''
-#print(arvore.buscar(70))
+#print(arvore.buscar(700))
 
 '''Quantidade de Nós'''
 #print(arvore.quantidadeFolhas)
@@ -235,6 +297,13 @@ arvore.menu()
 
 '''Maximo'''
 #print(f"Maximo: {arvore.maximo(arvore.raiz)}")
+
+"""Sucessor"""
+#arvore.sucessor(101)
+
+"""Antecessor"""
+#arvore.antecessor(70)
+
 
 '''Adicionar lista a Arvore'''
 #arvore.setList()
